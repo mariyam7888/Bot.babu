d787fe2d5fe19578c01ec65e6ad34523023ccffc
KRISHNA BOT MASTER <ravikumar630344222@gmail.com>
2025-10-16 12:10:46 +0530

    Update UP-TIME.js

--- models/commands/UP-TIME.js
+++ models/commands/UP-TIME.js
@@ -1,40 +1,69 @@
+const fetch = require('node-fetch');
+const os = require('os');
+const fs = require('fs');
+const language = process.env.REPL_LANGUAGE;
+const platform = os.platform();
+const architecture = os.arch();
+const cpuModel = os.cpus()[0].model;
+const uptime = os.uptime();
+const nodejs = process.version;
+global.client.timeStart = new Date().getTime();
+
 module.exports.config = {
-	name: "upt",
-	version: "1.0.2",
-	hasPermssion: 0,
-	credits: "Mirai Team",
-	description: "Kiểm tra thời gian bot đã online",
-	commandCategory: "system",
-	cooldowns: 5,
-	dependencies: {
-		"pidusage": ""
-	}
+  name: "upt",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "SHAAN KHAN",
+  description: "Koii Prefix nhi",
+  commandCategory: "Hukum Ke Bagher",
+  usages: "Online Time Timing Dekhye",
+  cooldowns: 5
 };
 
 function byte2mb(bytes) {
-	const units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-	let l = 0, n = parseInt(bytes, 10) || 0;
-	while (n >= 1024 && ++l) n = n / 1024;
-	return `${n.toFixed(n < 10 && l > 0 ? 1 : 0)} ${units[l]}`;
+  const units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  let l = 0, n = parseInt(bytes, 10) || 0;
+  while (n >= 1024 && ++l) n = n / 1024;
+  return `${n.toFixed(n < 10 && l > 0 ? 1 : 0)} ${units[l]}`;
 }
 
-module.exports.languages = {
-	"vi": {
-		"returnResult": "Bot đã hoạt động được %1 giờ %2 phút %3 giây.\n\n❯ Tổng người dùng: %4\n❯ Tổng Nhóm: %5\n❯ Cpu đang sử dụng: %6%\n❯ Ram đang sử dụng: %7\n❯ Ping: %8ms\n\n=== This bot was made by CatalizCS and SpermLord ==="
-	},
-	"en": {
-		"returnResult": "Bot has been working for %1 hour(s) %2 minute(s) %3 second(s).\n\n❯ Total users: %4\n❯ Total Threads: %5\n❯ Cpu usage: %6%\n❯ RAM usage: %7\n❯ Ping: %8ms\n\n=== This bot was made by CatalizCS and SpermLord ==="
-	}
-}
+module.exports.handleEvent = async ({ api, event, Threads }) => {
+  const xuly = Math.floor((Date.now() - global.client.timeStart) / 4444);
+  const trinhtrang = xuly < 10 ? "  Acha ✔️" : xuly > 10 && xuly < 100 ? "Thir" : "Ammi";
 
-module.exports.run = async ({ api, event, getText }) => {
-	const time = process.uptime(),
-		hours = Math.floor(time / (60 * 60)),
-		minutes = Math.floor((time % (60 * 60)) / 60),
-		seconds = Math.floor(time % 60);
+  if (!event.body) return;
 
-	const pidusage = await global.nodemodule["pidusage"](process.pid);
+  const { threadID, messageID } = event;
 
-	const timeStart = Date.now();
-	return api.sendMessage("", event.threadID, () => api.sendMessage(getText("returnResult", hours, minutes, seconds, global.data.allUserID.length, global.data.allThreadID.length, pidusage.cpu.toFixed(1), byte2mb(pidusage.memory), Date.now() - timeStart), event.threadID, event.messageID));
-} 
+  if (event.body.toLowerCase().indexOf("upt") == 0) {
+    const time = process.uptime(),
+          gio = Math.floor(time / (60 * 60)),
+          phut = Math.floor((time % (60 * 60)) / 60),
+          giay = Math.floor(time % 60);
+
+    const currentDate = new Date();
+    const formattedTime = currentDate.toLocaleTimeString('en-US', { 
+      hour12: true, 
+      timeZone: 'Asia/kolkata' 
+    });
+    const formattedDate = currentDate.toLocaleDateString('en-GB', { 
+      timeZone: 'Asia/kolkata' 
+    });
+    const formattedDay = currentDate.toLocaleDateString('en-US', { 
+      weekday: 'long', 
+      timeZone: 'Asia/kolkata' 
+    });
+
+    const responseMessage = `❁ ━━━[ 𝗨𝗣𝗧𝗜𝗠𝗘 ]━━━ ❁\n\n` +
+                            `❁ 𝗥𝗨𝗡 ➪ ${gio}ʜ ${phut}ᴍ ${giay}ꜱ\n` +
+                            `❁ 𝗧𝗜𝗠𝗘 ➪ ${formattedTime}\n` +
+                            `❁ 𝗗𝗔𝗧𝗘 ➪ ${formattedDate}\n` +
+                            `❁ 𝗗𝗔𝗬 ➪ ${formattedDay}\n` +
+                            `❁ ━━━━━━━━━━━━━━━ ❁\n` +
+                            `𝐎𝐖𝐍𝐄𝐑 :- 💖🌸💐𝗞𝗥𝗜𝗦𝗛𝗡𝗔`;
+
+    api.sendMessage(responseMessage, event.threadID, event.messageID);
+  }
+};
+
+module.exports.run = () => {};
